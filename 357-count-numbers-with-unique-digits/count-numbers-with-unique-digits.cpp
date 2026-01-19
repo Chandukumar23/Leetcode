145f7e6a49fdc208d735ca007d49061c24d7f5b0
Chandu Kumar <chandukumar5211@gmail.com>
2026-01-19 19:20:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (94.52%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+
+        int result = 10;      // for n = 1
+        int unique = 9;      // first digit (1–9)
+        int available = 9;   // remaining digits
+
+        for (int i = 2; i <= n && available > 0; i++) {
+            unique *= available;
+            result += unique;
+            available--;
+        }
+
+        return result;
+    }
+};
